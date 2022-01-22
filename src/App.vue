087c9a5616db62c3f9f71d6e54a6b8c8ae9316dc
Maksym Korchak <mkorchak1992@gmail.com>
2022-01-22 17:50:37 +0200

    Add functionality for rendering posts and creating new one

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,109 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="app">
+    <form @submit.prevent>
+      <h4>Create Post</h4>
+      <input
+        v-bind:value="title"
+        @input="title = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Post name ..."
+      />
+      <input
+        v-bind:value="body"
+        @input="body = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Post description ..."
+      />
+      <button @click="createPost">Save Post</button>
+    </form>
+    <div class="post" v-for="post in posts" :key="post.id">
+      <div><strong>Name: </strong>{{ post.title }}</div>
+      <div><strong>Description: </strong>{{ post.body }}</div>
+    </div>
   </div>
-  <router-view/>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+      body: "",
+      posts: [
+        {
+          id: 1,
+          title: "Post 1",
+          body: "Description for Post 1",
+        },
+        {
+          id: 2,
+          title: "Post 2",
+          body: "Description for Post 2",
+        },
+        {
+          id: 3,
+          title: "Post 3",
+          body: "Description for Post 3",
+        },
+      ],
+    };
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      };
+      this.posts.push(newPost);
+      this.title = "";
+      this.body = "";
+    },
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+.app {
+  padding: 20px;
 }
 
-#nav a {
+.post {
+  padding: 15px;
+  margin-top: 15px;
+  border: 2px solid teal;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+}
+
+form button {
+  cursor: pointer;
+  width: 100px;
+  padding: 10px;
+  outline: none;
+  border: 2px solid teal;
+  border-radius: 10px;
+  font-size: 14px;
   font-weight: bold;
-  color: #2c3e50;
+  background: none;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.input {
+  width: 50%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
 }
 </style>
