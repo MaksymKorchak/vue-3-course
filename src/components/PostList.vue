5258b7ceee912653b6a93b7851465bb3456f626e
Maksym Korchak <mkorchak1992@gmail.com>
2022-01-23 12:31:14 +0200

    Using animation transition group

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,14 +1,16 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>List</h3>
-    <post-item v-for="post in posts" 
-    :key="post.id" 
-    :post="post" 
-    @remove = "$emit('remove',post)"/>
+    <transition-group name="posts-list">
+      <post-item
+        v-for="post in posts"
+        :key="post.id"
+        :post="post"
+        @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
-  <h3 v-else>
-      There is no Items in the List ;(
-  </h3>
+  <h3 v-else>There is no Items in the List ;(</h3>
 </template>
 
 <script>
@@ -29,5 +31,22 @@ export default {
 h3 {
   padding: 15px 0;
   font-weight: bold;
+}
+
+.posts-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.posts-list-enter-active,
+.posts-list-leave-active {
+  transition: all 0.4s ease;
+}
+.posts-list-enter-from,
+.posts-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+.posts-list-move {
+  transition: transform 0.4s ease;
 }
 </style>
